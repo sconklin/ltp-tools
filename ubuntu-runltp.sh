69380804dc861f8202e945fead037cedbd69b372
Steve Conklin <sconklin@canonical.com>
2011-12-08 17:55:05 -0600

    Fixed the script

--- ubuntu-runltp.sh
+++ ubuntu-runltp.sh
@@ -3,14 +3,20 @@
 # This script runs the default ltp tests, capturing the output,
 # then processes that output to generate an xml report file
 
-TESTCASES=quickhit
-
-LTP_LOC=$PWD/ltp-full-20110915/ubuntu
-RUNSCRIPT=runltp
-SCRIPTARSG="-f $TESTCASES"
+TESTCASES=$*
+SCRIPTNAME=runltp
+LTP_LOC="$PWD/ltp-full-20110915/ubuntu"
+SCRIPTARGS="-f $TESTCASES"
 TFILE="./$(basename $0).$$.tmp"
 
-$LTPLOC/$RUNSCRIPT $SCRIPTARGS > $TFILE
+RS="$LTP_LOC/$SCRIPTNAME "
+
+echo Starting Test
+
+$RS $SCRIPTARGS | tee $TFILE
+
+echo Processing reults
 
 ./ltp2JUnit $TFILE > ltp-test.xml
 
+echo Done
