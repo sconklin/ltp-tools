fb2a033005b726872b7c4a7996de43bccee754ce
Steve Conklin <sconklin@canonical.com>
2011-12-09 21:02:00 -0600

    handle case of no tests specified

--- ubuntu-runltp.sh
+++ ubuntu-runltp.sh
@@ -3,20 +3,44 @@
 # This script runs the default ltp tests, capturing the output,
 # then processes that output to generate an xml report file
 
+XMLFILE="ltp-test.xml"
+
+BASEDIR="/src/ltp-tools"
+
 TESTCASES=$*
+echo "TESTCASES = $TESTCASES"
+if [ -s $TESTCASES ]; then
+    $TCARG="-f $TESTCASES";
+else
+    $TCARG=""
+fi
+
+echo "TCARG = $TCARG"
+
 SCRIPTNAME=runltp
-LTP_LOC="$PWD/ltp-full-20110915/ubuntu"
-SCRIPTARGS="-f $TESTCASES"
-TFILE="./$(basename $0).$$.tmp"
+LTP_LOC="$BASEDIR/ltp-full-20110915/ubuntu"
 
-RS="$LTP_LOC/$SCRIPTNAME "
+TEMPFILE="./$(basename $0).$$.tmp"
+TMPDIR="$PWD/tmpdir.$$"
+RESULTS_FILE="$PWD/result-log.$$"
+FAILED_FILE="$PWD/result-output.$$"
 
+SCRIPTARGS="$TCARG -l $RESULTS_FILE -C $FAILED_FILE -d $TMPDIR"
+RSCRIPT="$LTP_LOC/$SCRIPTNAME "
+
+echo "Temp file is $TEMPFILE"
+echo "Tmpdir is $TMPDIR"
 echo Starting Test
 
-$RS $SCRIPTARGS | tee $TFILE
+# make a local results directory
+#runltp -c 2 -i 2 -m 2,4,10240,1 -D 2,10,10240,1 -p -q  -l /tmp/result-log.25955 -o /tmp/result-output.25955 -C /tmp/result-failed.25955 -d /src/ltp-tools/ltp-full-20110915/ubuntu
+$RSCRIPT $SCRIPTARGS | tee $TEMPFILE
 
-echo Processing reults
+echo Processing results using $BASEDIR/ltp2JUnit
+rm $XMLFILE
+$BASEDIR/ltp2JUnit $TEMPFILE > $XMLFILE
 
-./ltp2JUnit $TFILE > ltp-test.xml
-
+echo removing tmp stuff
+#rm -rf $RESULTS_FILE $FAILED_FILE $OUTPUT_FILE $TMPDIR $TEMPFILE
+#rm -rf $RESULTS_FILE $FAILED_FILE $TMPDIR $TEMPFILE
 echo Done
